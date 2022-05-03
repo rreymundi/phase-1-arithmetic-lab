38c0a9765549c9a42c58950e6d0794c114f259bf
raul reymundi <kinskispiral@gmail.com>
2022-05-03 17:33:57 -0400

    commit tree f3ca73a951a66e0c7e13de95a36bb7cdf2b4d3ba

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 2;
 
-// Write your code here
+let num2 = 31;
+
+var multiply = num1 * num2;
+
+console.log(multiply);
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 40;
+
+let num4 = 6;
+
+var mod = num3 % num4;
+
+console.log(mod);
+
+var max = Math.max(4, 9, 10, 15, 20);
+
+console.log(max);
